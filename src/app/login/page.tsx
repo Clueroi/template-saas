dcd3aa6713dfb11b8f1826879d78298a12ac7141
clueroi <eriicsouzaromeroo@gmail.com>
2025-04-15 00:07:17 -0300

    Feat: Login with google

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,16 @@
-export default function Login(){
+import { HandleAuth } from "@/actions/handle-auth";
+
+export default function Login() {
   return (
     <div className="flex flex-col items-center justify-center h-screen" >
       <h1 className="text-4xl font-bold">
         Login
       </h1>
+      <form
+        action={HandleAuth}
+      >
+        <button type="submit">Signin with Google</button>
+      </form>
     </div>
   );
 }
