a4e50fb0c2692deea659d7679ef016c2734685fd
clueroi <eriicsouzaromeroo@gmail.com>
2025-04-16 16:38:24 -0300

    Fix: Little bugs fixed: PrivateKey changed to decodedKey, initializeApp wa not coming from the correct folder

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,6 +1,5 @@
 
-import { initializeApp } from 'firebase-admin'
-import { cert, getApps } from 'firebase-admin/app'
+import { cert, getApps, initializeApp } from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 // import { getStorage } from 'firebase-admin/storage'
 import "server-only"
@@ -14,7 +13,7 @@ const decodedKey = Buffer.from(process.env.FIREBASE_PRIVATE_KEY, 'base64').toStr
 export const firebaseCert = cert({
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY
+  privateKey: decodedKey
 })
 
 if(!getApps().length){
