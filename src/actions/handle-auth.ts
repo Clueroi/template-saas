50fc0f3092b12b10723fe024530df17fad572c07
clueroi <eriicsouzaromeroo@gmail.com>
2025-04-16 15:55:09 -0300

    Feat: Logout button

--- src/actions/handle-auth.ts
+++ src/actions/handle-auth.ts
@@ -1,8 +1,17 @@
 "use server"
 
-import { signIn } from "@/lib/auth"
+import { auth, signIn, signOut } from "@/lib/auth"
 
 export async function HandleAuth(){
+
+  const session = await auth()
+
+  if(session){
+    return await signOut({
+      redirectTo:'/login',
+    })
+  }
+
   await signIn("google", {
     redirectTo:"/dashboard",
   })
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,37 @@
+import { HandleAuth } from "@/actions/handle-auth"
 import { auth } from "@/lib/auth"
 import { redirect } from "next/navigation"
 
 
-export default async function Dashboard(){
+export default async function Dashboard() {
 
   const session = await auth()
-  
-  if(!session){
+
+  if (!session) {
     redirect("/login")
   }
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Email do usuário: {session?.user?.email ? session?.user?.email : 'Usuário não está logado'}</p>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p>
+        {
+          session?.user?.email
+            ? session?.user?.email
+            : 'Usuário não está logado'
+        }
+        {
+          session?.user?.email && (
+            <form
+              action={HandleAuth}
+            >
+              <button className="border-amber-50 border-2 rounded-md p-2 cursor-pointer" type="submit">
+                Logout
+              </button>
+            </form>
+          )
+        }
+      </p>
     </div>
   )
 }
