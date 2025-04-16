98c285275c7cd81a253667ad103aa4c2f4094844
clueroi <eriicsouzaromeroo@gmail.com>
2025-04-16 16:20:26 -0300

    Feat: Firebase added and configured to run the db

---
+++ src/lib/firebase.ts
@@ -0,0 +1,23 @@
+
+import { initializeApp } from 'firebase-admin'
+import { cert, getApps } from 'firebase-admin/app'
+import { getFirestore } from 'firebase-admin/firestore'
+// import { getStorage } from 'firebase-admin/storage'
+import "server-only"
+
+const firebaseCert = cert({
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+})
+
+if(!getApps().length){
+  initializeApp({
+    credential: firebaseCert,
+    // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  })
+  
+}
+
+export const db = getFirestore()
+// export const storage = getStorage().bucket()
