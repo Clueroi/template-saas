1942a7bb69150469e63e02f7d298637530bbb46b
clueroi <eriicsouzaromeroo@gmail.com>
2025-04-17 14:43:14 -0300

    Feat: Create Stripe Payment Hook

---
+++ src/hooks/useStripe.ts
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+export function useStripe() {
+  const [stripe, setStripe] = useState<Stripe | null>(null)
+
+  useEffect(() => {
+    async function loadStripeAsync() {
+
+      if (!process.env.NEXT_PUBLIC_STRIPE_PUB_KEY) {
+        throw new Error('Variable not defined')
+      }
+
+      const stripeInstance = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUB_KEY)
+
+      setStripe(stripeInstance)
+    }
+
+    loadStripeAsync()
+  }, [])
+
+  async function createPaymentStripeCheckout(checkoutData: any) {
+    if (!stripe) {
+      throw new Error('Stripe not loaded')
+    }
+
+    try {
+
+      const response = await fetch('/api/stripe/create-pay-checkout', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(checkoutData),
+      })
+
+      const data = await response.json()
+
+      await stripe.redirectToCheckout({ sessionId: data.id })
+
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async function createSubscriptionStripeCheckout(checkoutData: any) {
+    if (!stripe) return
+
+    try {
+      const response = await fetch('/api/stripe/create-subscription-checkout', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(checkoutData),
+      })
+      const data = await response.json()
+
+      await stripe.redirectToCheckout({sessionId: data.id})
+
+    } catch (err) {
+
+    }
+  }
+
+  async function handleCreateStripePortal(){
+    if (!stripe) return
+
+    try {
+      const response = await fetch('/api/stripe/create-portal', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+      const data = await response.json()
+
+      window.location.href = data.url
+
+    } catch (err) {
+
+    }
+  }
+
+  return {
+    createPaymentStripeCheckout,
+    createSubscriptionStripeCheckout,
+    handleCreateStripePortal
+  }
+}
